cdfc78bc22f0d1ad384845ec6d94ee6d5c9ac812
Shamiha Nawar Tanaj <shamihanawar685@gmail.com>
2026-07-21 22:02:16 +0600

    Revert "Feature008"

--- main.c
+++ main.c
@@ -1,22 +1,7 @@
-#include <stdio.h>
-#include "gpa.h"
+ #include <stdio.h>
 
-int main()
-{
-    double current_cgpa, completed_credits, expected_gpa, future_credits;
-
-    printf("Current CGPA: ");
-    scanf("%lf", &current_cgpa);
-    printf("Completed credits: ");
-    scanf("%lf", &completed_credits);
-    printf("Expected future GPA: ");
-    scanf("%lf", &expected_gpa);
-    printf("Future credits: ");
-    scanf("%lf", &future_credits);
-
-    printf("Expected CGPA: %.2f\n",
-           calculateExpectedCGPA(current_cgpa, completed_credits,
-                                 expected_gpa, future_credits));
-
-    return 0;
-}
+ int main()
+ {
+ printf("In IUT, CGPA Matters!\n");
+ return 0;
+ }
--- make.ps1
+++ make.ps1
@@ -1,1 +1,1 @@
-gcc -I modules main.c modules/course.c modules/courseResult.c modules/gpa.c -o calculator.exe
+gcc main.c -o calculator.exe
