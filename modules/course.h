da96968e2c595c76bc8b97f916e4ae0779e860a9
s-nawar <shamihanawar685@gmail.com>
2026-07-21 15:00:26 +0600

    Declared semester course helpers

--- modules/course.h
+++ modules/course.h
@@ -1,14 +1,18 @@
 #ifndef COURSE_H
 #define COURSE_H
 
- typedef struct Course
- {
- char code[16];
- char name[100];
- double credit;
- } Course;
+typedef struct Course
+{
+    char code[16];
+    char name[100];
+    double credit;
+    int semester;
+} Course;
 
- Course createCourse(char code[], char name[], double credit);
- void viewCourse(Course course);
+extern const char null_course_code[];
 
- #endif
+Course createCourse(char code[], char name[], double credit, int semester);
+int countCoursesBeforeNull(Course courses[], int n_courses);
+void viewCourse(Course course);
+
+#endif
